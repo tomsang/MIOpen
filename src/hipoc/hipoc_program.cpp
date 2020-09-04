asleepzzz/nhwc_shuffle

Squash all igemm-upstream-bkp-v1 changes into one commit

src/hipoc/hipoc_program.cpp
@@ -36,6 +36,7 @@
 #include <miopen/write_file.hpp>
 #include <miopen/env.hpp>
 #include <miopen/comgr.hpp>
+#include <miopen/logger.hpp>
 #include <boost/optional.hpp>
 
 #include <cstring>
@@ -151,6 +152,8 @@ struct HIPOCProgramImpl
     void
     BuildCodeObjectInFile(std::string& params, const std::string& src, const std::string& filename)
     {
+        MIOPEN_LOG_I("BuildCodeObjectInFile for file: " << filename);
+
         dir.emplace(filename);
         hsaco_file = dir->path / (filename + ".o");
 
@@ -182,6 +185,8 @@ struct HIPOCProgramImpl
                                  const std::string& src,
                                  const std::string& filename)
     {
+        MIOPEN_LOG_I("BuildCodeObjectInMemory for file: " << filename);
+
         if(miopen::EndsWith(filename, ".so"))
         {
             std::size_t sz = src.length();
@@ -210,10 +215,27 @@ struct HIPOCProgramImpl
     {
         std::string filename = is_kernel_str ? "tinygemm.cl" // Fixed name for miopengemm.
                                              : program;
-        const std::string src =
-            !kernel_src.empty() ? kernel_src : is_kernel_str ? program : GetKernelSrc(program);
+        // GetKernelSrc will consult miopen_kernels.h for ascii version of
+        // kernel, therefore result in a failure. However, the mlir version
+        // of hipbuild does not care about what src is. Replacing it with
+        // an assignment now to be fixed later
+        std::string src;
+        if(!kernel_src.empty())
+        {
+            src = kernel_src;
+        }
+        else if(program.find("mlir") != std::string::npos)
+        {
+            // For MLIR path, leave the kernel_src to be empty.
+            src = kernel_src;
+        }
+        else
+        {
+            src = is_kernel_str ? program : GetKernelSrc(program);
+        }
 
-        if(miopen::EndsWith(filename, ".cpp"))
+        if(program.find("mlir") != std::string::npos) {}
+        else if(miopen::EndsWith(filename, ".cpp"))
         {
 #if MIOPEN_BUILD_DEV
             params += " -Werror" + HipKernelWarningsString();

tomsang/MIOpen

src/hipoc/hipoc_program.cpp
@@ -36,6 +36,7 @@
 #include <miopen/write_file.hpp>
 #include <miopen/env.hpp>
 #include <miopen/comgr.hpp>
+#include <miopen/logger.hpp>
 #include <boost/optional.hpp>
 
 #include <cstring>
@@ -151,6 +152,8 @@ struct HIPOCProgramImpl
     void
     BuildCodeObjectInFile(std::string& params, const std::string& src, const std::string& filename)
     {
+        MIOPEN_LOG_I("BuildCodeObjectInFile for file: " << filename);
+
         dir.emplace(filename);
         hsaco_file = dir->path / (filename + ".o");
 
@@ -182,6 +185,8 @@ struct HIPOCProgramImpl
                                  const std::string& src,
                                  const std::string& filename)
     {
+        MIOPEN_LOG_I("BuildCodeObjectInMemory for file: " << filename);
+
         if(miopen::EndsWith(filename, ".so"))
         {
             std::size_t sz = src.length();
@@ -210,10 +215,29 @@ struct HIPOCProgramImpl
     {
         std::string filename = is_kernel_str ? "tinygemm.cl" // Fixed name for miopengemm.
                                              : program;
-        const std::string src =
-            !kernel_src.empty() ? kernel_src : is_kernel_str ? program : GetKernelSrc(program);
+        // GetKernelSrc will consult miopen_kernels.h for ascii version of
+        // kernel, therefore result in a failure. However, the mlir version
+        // of hipbuild does not care about what src is. Replacing it with
+        // an assignment now to be fixed later
+        std::string src;
+        if(!kernel_src.empty())
+        {
+            src = kernel_src;
+        }
+        else if(program.find("mlir_gen_igemm_conv2d") != std::string::npos)
+        {
+            // For MLIR path, leave the kernel_src to be empty.
+            src = kernel_src;
+        }
+        else
+        {
+            src = is_kernel_str ? program : GetKernelSrc(program);
+        }
 
-        if(miopen::EndsWith(filename, ".cpp"))
+        if(program.find("mlir_gen_igemm_conv2d") != std::string::npos)
+        {
+        }
+        else if(miopen::EndsWith(filename, ".cpp"))
         {
 #if MIOPEN_BUILD_DEV
             params += " -Werror" + HipKernelWarningsString();